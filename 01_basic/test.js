a353e37526b9a026347ad05edacd087f8bc59a33
Mousam kumari <133010326+MousamKumari13@users.noreply.github.com>
2024-12-06 16:27:46 +0000

    container config done

---
+++ 01_basic/test.js
@@ -0,0 +1,11 @@
+const accountId = 123
+let accountEmail = "memousam@.com"
+var accountPassword =  "1982"
+accountCity = "jaipur"
+
+accountEmail = "mousam@12.com"
+accountPassword = "876"
+accountCity = "Bihar"
+
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword ,accountCity]);
