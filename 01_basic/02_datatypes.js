fb80d9d34012c20274cc0c54c8da635dcb4e797a
Mousam kumari <133010326+MousamKumari13@users.noreply.github.com>
2024-12-06 16:53:33 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all JS code as newer version
+
+let name =" mousam" // string
+let age =  22
+let islogin = true // boolean
+
+//number 2 to power 53
+//bigint
+//string = " "
+//bollean = true/false
+//null = standlone value
+// undefined 
+//symbol = unique
+
+console.log(typeof null);// object
+console.log(typeof undefined); // undefined
