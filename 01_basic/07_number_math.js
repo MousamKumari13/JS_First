a72b9f3ce777eef3448e2c757ee330639c59ec71
Mousam kumari <133010326+MousamKumari13@users.noreply.github.com>
2024-12-10 13:21:12 +0000

    math_num

--- 01_basic/07_number_math.js
+++ 01_basic/07_number_math.js
@@ -21,23 +21,23 @@ console.log(hundred.toLocaleString('en-IN')); //10,00,000
 //+++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++
 
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.min(4 , 2 , 3 , 6));
-console.log(Math.max(4 , 2 , 3 , 6));
+console.log(Math); // output - Object [Math] {}
+console.log(Math.abs(-4)); // 4
+console.log(Math.round(4.6)); //5
+console.log(Math.ceil(4.2));// 5
+console.log(Math.floor(4.9));// 4
+console.log(Math.min(4 , 2 , 3 , 6)); //2
+console.log(Math.max(4 , 2 , 3 , 6)); // 6
 
 
-console.log(Math.random()); //this gives value between 0 and 1
-console.log((Math.random() * 10) + 1); // adding 1 for avoiding 0 as output
-console.log(Math.floor((Math.random() * 10) + 1));
+console.log(Math.random()); //this gives value between 0 and 1 , output - 0.7448120766961686
+console.log((Math.random() * 10) + 1); // adding 1 for avoiding 0 as output - 3.3379706974987013
+console.log(Math.floor((Math.random() * 10) + 1)); //3
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1) ) + min);
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min); // 19
 
 
 
