f8b0d5df8f73d3c60b2d870ea4a1dcd0051e2d1e
Mousam kumari <133010326+MousamKumari13@users.noreply.github.com>
2024-12-20 17:28:04 +0000

    if_else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+const temprature = 41
+
+if(temprature == 40){
+    console.log("Temperature is less than 50");
+    
+}else{
+    console.log("temperature is greater than 50");
+    
+}
+console.log("Execute");
+
+const score = 200
+if(score > 100){
+    var power = "fly" // we cant use var because it also  acess by outside the scope 
+    console.log(`user power : ${power}`);
+    
+}
+console.log(`user power : ${power}`);
