3c5a68f41ad73b812a62fdf9cc2b0cfbdccdafd7
Mousam kumari <133010326+MousamKumari13@users.noreply.github.com>
2024-12-22 18:12:02 +0000

    while_loop

---
+++ 05_Loops/while_do.js
@@ -0,0 +1,13 @@
+let myarr = ['Mousam' , "Rakhi" , "Sonu"]
+let arr=0;
+while(arr< myarr.length){
+    console.log(myarr[arr]);
+    arr += 1
+}
+
+let score = 1;
+do {
+    console.log(score);
+    score++
+    
+} while (score < 10);
