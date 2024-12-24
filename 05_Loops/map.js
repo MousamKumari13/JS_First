230169ee6fce96b824f3bc98ee58c4eec74a9e05
Mousam kumari <133010326+MousamKumari13@users.noreply.github.com>
2024-12-24 17:08:32 +0000

    map+filter

---
+++ 05_Loops/map.js
@@ -0,0 +1,20 @@
+const mynum = [ 1 , 2 , 3, 4, 5, 6, 7 , 8, 9, 10]
+
+const newNums = mynum.map( (num) => { return (num + 10)})
+
+console.log(newNums); /*[
+  11, 12, 13, 14, 15,
+  16, 17, 18, 19, 20
+]
+*/
+const newnum = mynum
+               .map( (num) => num * 10)
+               .map( (num) => num + 1)
+               .filter( (num) => num >= 40)
+               
+console.log(newnum); /*
+[
+  41, 51,  61, 71,
+  81, 91, 101
+]
+*/
